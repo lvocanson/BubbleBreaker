04ce9f7fd075825af73caab0d56cac3f7cd53fb5
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2024-12-19 09:35:27 +0100

    [FIX] corner collision working

--- src/Game/Ball.cpp
+++ src/Game/Ball.cpp
@@ -1,9 +1,10 @@
 #include "Ball.h"
 #include "Core/Resources.h"
 #include "Core/ResourcesManager.h"
+#include "Utils/Logger.h"
 
 Ball::Ball(sf::Vector2f position)
-	: EntityWithVelocity(ResourcesManager::Instance().GetTexture(Resources::BallTexture), {position, Resources::BallSize})
+	: EntityWithVelocity(ResourcesManager::Instance().GetTexture(Resources::BallTexture), { position, Resources::BallSize })
 {
 }
 
@@ -15,11 +16,12 @@ bool Ball::ReactToCollision(sf::FloatRect rectangle)
 		rectangle.position.y + rectangle.size.y / 2
 	};
 
-	const float radius = m_Sprite.getGlobalBounds().size.x / 2;
+	sf::FloatRect ballBounds = m_Sprite.getGlobalBounds();
+	const float radius = ballBounds.size.x / 2;
 	sf::Vector2f ballPos
 	{
-		m_Sprite.getGlobalBounds().position.x + radius,
-		m_Sprite.getGlobalBounds().position.y + radius
+		ballBounds.position.x + radius,
+		ballBounds.position.y + radius
 	};
 
 	sf::Vector2f distance
@@ -36,22 +38,56 @@ bool Ball::ReactToCollision(sf::FloatRect rectangle)
 
 	if (cornerDistance_sq <= radius * radius)
 	{
-		m_Velocity *= -1.f;
-		if (m_Velocity.lengthSquared() < Resources::MaxBallSpeed * Resources::MaxBallSpeed)
-			m_Velocity *= Resources::BounceSpeedFactor;
+		//set BallPos relative to rectPos
+		ballPos -= rectOrigin;
+
+		const float ballSpeed = m_Velocity.length();
+		// find the distance that the corner is from the line segment from the balls pos to the next pos
+		const float u = (((rectangle.size.x / 2) - ballPos.x) * m_Velocity.x + ((rectangle.size.y / 2) - ballPos.y) * m_Velocity.y) / (ballSpeed * ballSpeed);
+
+		// get the closest point on the line to the corner
+		float cpx = ballPos.x + m_Velocity.x * u;
+		float cpy = ballPos.y + m_Velocity.y * u;
+
+		Logger::Instance() << cpx << " ; " << cpy << '\n';
+		// get the distance of that point from the corner squared
+		const float dist = (cpx - (rectangle.size.x / 2)) * (cpx - (rectangle.size.x / 2)) + (cpy - (rectangle.size.y / 2)) * (cpy - (rectangle.size.y / 2));
+
+
+		// solves the triangle from center to closest point on balls trajectory
+		float d = sqrt(abs((radius * radius) - dist)) / ballSpeed;
+
+		cpx -= m_Velocity.x * d;
+		cpy -= m_Velocity.y * d;
+
+		Logger::Instance() << cpx << " ; " << cpy << '\n';
+		// get the distance from the ball current pos to the intercept point
+		d = sqrt((cpx - ballPos.x) * (cpx - ballPos.x) + (cpy - ballPos.y) * (cpy - ballPos.y));
+
+		// find the normalised tangent at intercept point 
+		const float ty = (cpx - (rectangle.size.x / 2)) / radius;
+		const float tx = -(cpy - (rectangle.size.y / 2)) / radius;
+
+		m_Velocity = m_Velocity.normalized();
+
+		//reflectionVector
+		const float dot = (m_Velocity.x * tx + m_Velocity.y * ty) * 2;
+
+		//distance travelled past intercept
+		d = ballSpeed - d;
+
+		m_Velocity.x = (tx * dot - m_Velocity.x);
+		m_Velocity.y = (ty * dot - m_Velocity.y);
+		m_Velocity = m_Velocity.normalized();
+
+		m_Velocity *= ballSpeed;
+
 		return true;
 	}
 
 	if (distance.x <= (rectangle.size.x / 2) + radius && distance.x >= (rectangle.size.x / 2) - radius)
 	{
 		m_Velocity.x *= -1;
-		int sign = m_Velocity.x > 0 ? 1 : -1;
-		sf::Vector2f offset
-		{
-			abs(rectangle.size.x / 2 - distance.x) * sign,
-			0.f
-		};
-		//m_Sprite.move(offset);
 		if (m_Velocity.lengthSquared() < Resources::MaxBallSpeed * Resources::MaxBallSpeed)
 			m_Velocity *= Resources::BounceSpeedFactor;
 		return true;
@@ -59,14 +95,7 @@ bool Ball::ReactToCollision(sf::FloatRect rectangle)
 
 	if (distance.y <= (rectangle.size.y / 2) + radius && distance.y >= (rectangle.size.y / 2) - radius)
 	{
-		int sign = m_Velocity.y > 0 ? -1 : 1;
 		m_Velocity.y *= -1;
-		sf::Vector2f offset
-		{
-			0.f,
-			abs(rectangle.size.y / 2 - distance.y) * sign,
-		};
-		//m_Sprite.move(offset);
 		if (m_Velocity.lengthSquared() < Resources::MaxBallSpeed * Resources::MaxBallSpeed)
 			m_Velocity *= Resources::BounceSpeedFactor;
 		return true;
